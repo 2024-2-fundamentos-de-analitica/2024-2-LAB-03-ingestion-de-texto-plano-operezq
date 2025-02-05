13567252b0ed19ab815db507f08fc23f9a9903a2
Oscar Pérez <operezq@unal.edu.co>
2025-02-05 11:26:58 -0500

    Laboratorio completo

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -4,6 +4,7 @@ Escriba el codigo que ejecute la accion solicitada en cada pregunta.
 
 # pylint: disable=import-outside-toplevel
 
+import pandas as pd
 
 def pregunta_01():
     """
@@ -18,3 +19,27 @@ def pregunta_01():
 
 
     """
+
+    with open("files/input/clusters_report.txt", "r") as file:
+        texto = file.readlines()
+        info = []
+        dataset = {}
+        for linea in texto[4:]:
+            if linea.strip():  
+              columna= linea.split()
+              if columna[0].isdigit():
+                dataset = {
+                    "cluster" : int(columna[0]),
+                    "cantidad_de_palabras_clave": int(columna[1]),
+                    "porcentaje_de_palabras_clave": float(columna[2].replace(",",".")),
+                    "principales_palabras_clave": " ".join(columna[4:]).rstrip(".")
+                    }
+                info.append(dataset)
+                
+              else:
+                 dataset["principales_palabras_clave"] += ' ' + ' '.join(columna).rstrip(".")
+
+
+        df = pd.DataFrame(info)
+
+    return df
